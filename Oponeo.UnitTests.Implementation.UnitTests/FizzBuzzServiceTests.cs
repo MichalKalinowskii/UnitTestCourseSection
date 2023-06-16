765489af80c352e264249927cb6282e820bdfd69
Michal Kalinowski <michal.kalinowski@oponeo.pl>
2023-06-16 10:41:55 +0200

    Add project files.

---
+++ Oponeo.UnitTests.Implementation.UnitTests/FizzBuzzServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Oponeo.UnitTests.Implementation.UnitTests
+{
+    [TestClass]
+    public class FizzBuzzServiceTests
+    {
+        [TestMethod]
+        [DataRow(9, "Fizz")]
+        [DataRow(10, "Buzz")]
+        [DataRow(120, "FizzBuzz")]
+        [DataRow(2, "2")]
+        public void Should_Return_Correct_Result(int n, string reuslt)
+        {
+            var fizzBuzzService = new FizzBuzzService();
+
+            string res = fizzBuzzService.FizzBuzzGenerator(n);
+
+            Assert.AreEqual(res, reuslt);
+        }
+
+        [TestMethod]
+        [DataRow(10, "Fizz")]
+        [DataRow(9, "Buzz")]
+        [DataRow(2, "FizzBuzz")]
+        public void Should_Return_Incorrect_Result(int n, string reuslt)
+        {
+            var fizzBuzzService = new FizzBuzzService();
+
+            string res = fizzBuzzService.FizzBuzzGenerator(n);
+
+            Assert.AreNotEqual(res, reuslt);
+        }
+    }
+}
---
+++ Oponeo.UnitTests.Implementation/FizzBuzzService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oponeo.UnitTests.Implementation
+{
+    public class FizzBuzzService
+    {
+        public string FizzBuzzGenerator(int n)
+        {
+            bool canDivideBy3 = n % 3 == 0;
+            bool canDivideBy5 = n % 5 == 0;
+
+            if (canDivideBy3 && canDivideBy5) return "FizzBuzz";
+            if (canDivideBy3) return "Fizz";
+            if (canDivideBy5) return "Buzz";
+
+            return n.ToString();
+        }
+    }
+}
