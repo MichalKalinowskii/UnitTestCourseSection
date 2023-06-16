621fd3fa20af09bf8e15f1421de8c7157a272d15
Michal Kalinowski <michal.kalinowski@oponeo.pl>
2023-06-16 14:20:53 +0200

    calculator

---
+++ Oponeo.UnitTests.Implementation.UnitTests/PriceCalculatorTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oponeo.UnitTests.Implementation.UnitTests
+{
+    [TestClass]
+    public class PriceCalculatorTest
+    {
+
+        [TestMethod]
+        [DataRow(10, 80, 1, 7)]
+        [DataRow(100, 66, 3, 70)]
+        [DataRow(35, 120, 8000, 35 * 0.7)]
+        [DataRow(77, 90, 800, 77 * 0.7)]
+        public void Should_Return_30_Discount(int price, int age, int daysFromLastVisit, double expectedPrice)
+        {
+            var calculator = new PriceCalculator();
+
+            double res = calculator.CalculatePrice(price, age, daysFromLastVisit);
+
+            Assert.AreEqual(expectedPrice, res);
+        }
+
+        [TestMethod]
+        [DataRow(10, 30, 200, 5)]
+        [DataRow(100, 50, 201, 50)]
+        [DataRow(35, 40, 351, 35 * 0.5)]
+        [DataRow(77, 13, 685, 77 * 0.5)]
+        public void Should_Return_50_Discount(int price, int age, int daysFromLastVisit, double expectedPrice)
+        {
+            var calculator = new PriceCalculator();
+
+            double res = calculator.CalculatePrice(price, age, daysFromLastVisit);
+
+            Assert.AreEqual(expectedPrice, res);
+        }
+
+        [TestMethod]
+        [DataRow(10, 0, 200, 0)]
+        [DataRow(100, 1, 201, 0)]
+        [DataRow(35, 2, 351, 0)]
+        [DataRow(77, 3, 685, 0)]
+        public void Should_Return_100_Discount(int price, int age, int daysFromLastVisit, double expectedPrice)
+        {
+            var calculator = new PriceCalculator();
+
+            double res = calculator.CalculatePrice(price, age, daysFromLastVisit);
+
+            Assert.AreEqual(expectedPrice, res);
+        }
+    }
+}
---
+++ Oponeo.UnitTests.Implementation/PriceCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oponeo.UnitTests.Implementation
+{
+    public class PriceCalculator
+    {
+        public PriceCalculator()
+        {
+
+        }
+
+        public double CalculatePrice(int price, int age, int daysFromLastVisit) 
+        {
+            if (age > 65) return price * 0.7;
+            if (age < 4 && age >= 0) return 0;
+            if (daysFromLastVisit >= 200) return price * 0.5;
+            return price;
+        }
+
+    }
+}
