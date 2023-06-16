8974a16905062ac4d32d6c31f56a429fed435116
Michal Kalinowski <michal.kalinowski@oponeo.pl>
2023-06-16 12:26:15 +0200

    fibonnaci

---
+++ Oponeo.UnitTests.Implementation/FibonnaciService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oponeo.UnitTests.Implementation
+{
+    public class FibonnaciService
+    {
+        public int GetValueOnIndexInFibonnaci(int index)
+        {
+            if (index < 0) throw new ArgumentOutOfRangeException();
+            if (index == 0 || index == 1) return 1;
+
+            
+            return GetValueOnIndexInFibonnaci(index - 1) + GetValueOnIndexInFibonnaci(index - 2);
+
+        }
+    }
+}
