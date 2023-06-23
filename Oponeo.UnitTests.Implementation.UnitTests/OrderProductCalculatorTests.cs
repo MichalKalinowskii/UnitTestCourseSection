18b862eb2eed96e053def49bdd4486f36250abd6
Michal Kalinowski <michal.kalinowski@oponeo.pl>
2023-06-23 08:19:31 +0200

    TestInitialize

--- Oponeo.UnitTests.Implementation.UnitTests/OrderProductCalculatorTests.cs
+++ Oponeo.UnitTests.Implementation.UnitTests/OrderProductCalculatorTests.cs
@@ -10,32 +10,39 @@ namespace Oponeo.UnitTests.Implementation.UnitTests
     [TestClass]
     public class OrderProductCalculatorTests
     {
+        [TestInitialize]
+        public List<OrderProduct> Products()
+        {
+            List<OrderProduct> products = new()
+            {
+                new OrderProduct
+                {
+                    UnitPrice = 10,
+                    Quantity = 1
+                },
+                new OrderProduct
+                {
+                    UnitPrice = 12.5,
+                    Quantity = 8
+
+                },
+                new OrderProduct
+                {
+                    UnitPrice = 3.5,
+                    Quantity = 48
+                }
+            };
+
+            return products;
+        }
 
         [TestMethod]
         public void Should_Return_200Sum_And_One_10Quantity_Discount()
         {
-            List<OrderProduct> products = new();
-            products.Add(new OrderProduct
-            {
-                UnitPrice = 10,
-                Quantity = 1
-            });
-            products.Add(new OrderProduct
-            {
-                UnitPrice = 12.5,
-                Quantity = 8
 
-            });
-            products.Add(new OrderProduct
-            {
-                UnitPrice = 3.5,
-                Quantity = 48
-            });
-
-            OrderProductCalculator calc = new(products);
+            OrderProductCalculator calc = new(Products());
 
             double sum = calc.CalculateOrderPrice();
-
 
             Assert.AreEqual(235.08, Math.Round(sum,2));
         }
