bb96178d1bf6e176e02f13f0337d539b46afa8ed
Michal Kalinowski <michal.kalinowski@oponeo.pl>
2023-06-23 07:59:36 +0200

    OrderProductCalculator

---
+++ Oponeo.UnitTests.Implementation/OrderProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oponeo.UnitTests.Implementation
+{
+    public class OrderProduct
+    {
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        public double Quantity { get; set; }
+        public double TotalPrice => UnitPrice * Quantity;
+    }
+}
---
+++ Oponeo.UnitTests.Implementation/OrderProductCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oponeo.UnitTests.Implementation
+{
+    public class OrderProductCalculator
+    {
+
+        private List<OrderProduct> _product;
+
+        public OrderProductCalculator(List<OrderProduct> products)
+        {
+            _product = products;
+        }
+
+        public double CalculateOrderPrice()
+        {
+            double sum = _product.Sum(p => p.Quantity * p.UnitPrice);
+
+            foreach(var product in _product)
+            {
+                if (sum > 200)
+                    product.UnitPrice *= 0.9;
+
+                if (sum > 400)
+                    product.UnitPrice *= 0.8;
+
+                if (product.Quantity > 10)
+                    product.UnitPrice *= 0.9;
+            }
+
+            return _product.Sum(p => p.Quantity * p.UnitPrice);
+        }
+    }
+}
